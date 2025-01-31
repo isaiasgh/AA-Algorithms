74edc0bc07b814b2c41371e1ee6b7ec16e19506c
isaiasgh <joguherr@espol.edu.ec>
2025-01-31 00:16:00 -0500

    Implement Quicksort in Java

---
+++ Sorting/Quicksort/Quicksort.java
@@ -0,0 +1,11 @@
+package Sorting.Quicksort;
+
+public class Quicksort {
+    public static void quicksort(int[] A, int p, int r) {
+        if (p < r) {
+            int q = Partition.partition(A, p, r);
+            quicksort(A, p, q - 1);
+            quicksort(A, q + 1, r);
+        }
+    }
+}
