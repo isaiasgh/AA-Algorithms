1ef8d00ace66ba251d1e88c0f5aa188a0043ee1c
isaiasgh <joguherr@espol.edu.ec>
2025-01-31 00:19:30 -0500

    Refactor: modify Quicksort.py to use imported Partition function

--- Sorting/Quicksort/Quicksort.py
+++ Sorting/Quicksort/Quicksort.py
@@ -1,17 +1,7 @@
+from Sorting.Quicksort.Partition import Partition
+
 def Quicksort (A, p, r):
     if p < r:
         q = Partition (A, p, r)
         Quicksort (A, p, q - 1)
         Quicksort (A, q + 1, r)
-
-def Partition (A, p, r):
-    x = A[r]
-    i = p - 1
-
-    for j in range (p, r):
-        if A [j] <= x:
-            i += 1
-            A[i], A[j] = A[j], A[i]
-    
-    A[i + 1], A[r] = A[r], A[i + 1]
-    return i + 1
